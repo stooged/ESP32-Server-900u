9f4289e294c901caf5b2d54e8f6c37a6cb140e23
stooged <stooged@users.noreply.github.com>
2022-02-22 08:40:31 +0800

    Update ESP32_Server_900u.ino
    remove unused function

--- ESP32_Server_900u/ESP32_Server_900u.ino
+++ ESP32_Server_900u/ESP32_Server_900u.ino
@@ -182,25 +182,6 @@ String urlencode(String str)
 }
 
 
-String getContentType(String filename){
-  if(filename.endsWith(".htm")) return "text/html";
-  else if(filename.endsWith(".html")) return "text/html";
-  else if(filename.endsWith(".css")) return "text/css";
-  else if(filename.endsWith(".js")) return "text/javascript";
-  else if(filename.endsWith(".png")) return "image/png";
-  else if(filename.endsWith(".gif")) return "image/gif";
-  else if(filename.endsWith(".jpg")) return "image/jpeg";
-  else if(filename.endsWith(".ico")) return "image/x-icon";
-  else if(filename.endsWith(".xml")) return "text/xml";
-  else if(filename.endsWith(".pdf")) return "application/x-pdf";
-  else if(filename.endsWith(".zip")) return "application/x-zip";
-  else if(filename.endsWith(".gz")) return "application/x-gzip";
-  else if(filename.endsWith(".bin")) return "application/octet-stream";
-  else if(filename.endsWith(".manifest")) return "text/cache-manifest";
-  return "text/plain";
-}
-
-
 void sendwebmsg(AsyncWebServerRequest *request, String htmMsg)
 {
     String tmphtm = "<!DOCTYPE html><html><head><link rel=\"stylesheet\" href=\"style.css\"></head><center><br><br><br><br><br><br>" + htmMsg + "</center></html>";
